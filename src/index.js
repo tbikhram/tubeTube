d8e6b327e65209d2cfcb906e527a25f8490f5c8d
tbikhram <t.bikhram@gmail.com>
2018-05-29 21:25:55 -0700

    updated package.json and added the search_bar component, the API key, and the search_bar component into the index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
+import SearchBar from './components/search_bar'
+// Api_key from console.developers.google.com
+const API_KEY = 'AIzaSyBiJgOpJnWJO9SNEeQiwA_8YbhzQ-IKeJ0';
+
 // 1.create a new component, should produce some HTML
 // 2.take this component's generated HTML and put it on the page (in the DOM)
 
 // 1.
 const App = () => {
-	return <div> hello there</div>;
+	return <div> 
+			<SearchBar />
+			</div>;
 }
 
 
