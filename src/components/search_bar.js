62c46ad29d9d60f101111cc3455a70c8e366c64d
tbikhram <t.bikhram@gmail.com>
2018-05-30 10:21:48 -0700

    added the state iniilization to the search_bar to render the text entered and update the event target value

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,8 +2,25 @@ import React, { Component } from 'react';
 
 
 class SearchBar extends Component {
+	// before using state inside of component you need t0 initalizing the state 
+	constructor(props){
+		super(props)
+		//the state term is empty(empty search bar) waiting for text to be entered thats
+		//why term: is an empty string
+		this.state = {term: ''}
+	}
 	render() {
-		return <input onChange = {event => console.log(event.target.value) } />;
+
+		return (
+			<div>
+		<input 
+		value={this.state.term}
+	//the state is updated when text is entered in the input box
+		onChange = {event => this.setState({term: event.target.value })} />
+
+		
+		</div>
+		)
 	}
 }
 
