cf67ad6c5495566d5c8ef69e5d3a4ae0140f3900
tbikhram <t.bikhram@gmail.com>
2018-05-30 10:41:51 -0700

    added a class component with a constructor to call videos and use the google API_KEY

--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom'
-
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'
 // Api_key from console.developers.google.com
 const API_KEY = 'AIzaSyBiJgOpJnWJO9SNEeQiwA_8YbhzQ-IKeJ0';
 
 // 1.create a new component, should produce some HTML
 // 2.take this component's generated HTML and put it on the page (in the DOM)
+// using youtube api to search for surfboards
+// YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+// 	console.log(data);
+// });
 
-// 1.
-const App = () => {
-	return <div> 
+
+class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+		 this.setState({ videos });
+ 	});
+}
+
+render(){
+	return(
+		<div>
 			<SearchBar />
-			</div>;
+		</div>
+		);
+
+	}
 }
 
 
