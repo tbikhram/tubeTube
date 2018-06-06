97da1bf1a67708797adbf9da2783825e0f6487a0
tbikhram <t.bikhram@gmail.com>
2018-06-05 19:23:02 -0700

    added the funcitonality of the video_detail to load a video with embeded info the is 16by 9 and video below it can be selected from the video_list

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-import SearchBar from './components/search_bar'
-import VideoList from './components/video_list'
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 // Api_key from console.developers.google.com
 const API_KEY = 'AIzaSyBiJgOpJnWJO9SNEeQiwA_8YbhzQ-IKeJ0';
 
@@ -19,10 +20,18 @@ class App extends Component {
 	constructor(props){
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [],
+			selectedVideo: null 
+
+		};
 
 		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-		 this.setState({ videos });
+		 this.setState({ 
+		 	videos: videos, 
+		 	selectedVideo: videos[0]
+
+		 });
  	});
 }
 
@@ -30,7 +39,10 @@ render(){
 	return(
 		<div>
 			<SearchBar />
-			<VideoList videos ={this.state.videos} />
+			<VideoDetail video={this.state.selectedVideo} />
+			<VideoList
+				onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+				videos ={this.state.videos} />
 		</div>
 		);
 
