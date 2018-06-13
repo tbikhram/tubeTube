8416d6954f26b335f1c6ec3c9b1b95a6c8ff650c
tbikhram <t.bikhram@gmail.com>
2018-06-13 16:01:27 -0700

    added lodash to the search bar so items do not constanly pop up for every letter and changed the onSearchTermChange to videoseach, also added a const videoSearch method with the _.debounce witbh 300 ms time delay for search imputs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -41,9 +42,11 @@ videoSearch(term){
 
 
 render(){
+	// lodash in use here
+	const videoSearch = _.debounce((term) =>{ this.videoSearch(term) }, 300);
 	return(
 		<div>
-			<SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+			<SearchBar onSearchTermChange={videoSearch} />
 			<VideoDetail video={this.state.selectedVideo} />
 			<VideoList
 				onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
