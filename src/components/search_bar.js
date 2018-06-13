567778487e4e0be553f57ab33e7c53c940d4bfa0
tbikhram <t.bikhram@gmail.com>
2018-06-13 14:47:08 -0700

    the styling for the video detail, img, search-bar with margins, bc, padding, size, border, hover and cursor style

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,7 +12,7 @@ class SearchBar extends Component {
 	render() {
 
 		return (
-			<div>
+			<div className="search-bar">
 		<input 
 		value={this.state.term}
 	//the state is updated when text is entered in the input box
